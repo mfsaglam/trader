0e8c5748c67871717af3a3eed0f70edd498f5613
mfsaglam <mfsaglam@yandex.com>
2022-06-14 23:59:41 +0300

    add rising bool and declining color for StockIndicatorView

--- trader/StockIndicatorView.swift
+++ trader/StockIndicatorView.swift
@@ -7,14 +7,10 @@
 
 import UIKit
 
-enum IndicatorCase {
-    case rising
-    case declining
-}
-
 class StockIndicatorView: UIView {
     
     var indicatorImage = UIImageView()
+    var isRising = false
 
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -27,22 +23,33 @@ class StockIndicatorView: UIView {
     
     private func configure() {
         
-        backgroundColor = .systemGreen
+        backgroundColor = isRising ? .systemGreen : .systemRed
         layer.cornerRadius = 5
         translatesAutoresizingMaskIntoConstraints = false
         
         self.addSubview(indicatorImage)
         indicatorImage.translatesAutoresizingMaskIntoConstraints = false
-        indicatorImage.image = UIImage(systemName: "chevron.compact.up", withConfiguration: UIImage.SymbolConfiguration(pointSize: 20, weight: .black))
         indicatorImage.contentMode = .scaleAspectFill
         indicatorImage.tintColor = .white
         
-        NSLayoutConstraint.activate([
-            indicatorImage.centerXAnchor.constraint(equalTo: self.centerXAnchor),
-            indicatorImage.topAnchor.constraint(equalTo: self.topAnchor,constant: 4),
-            indicatorImage.heightAnchor.constraint(equalToConstant: 20),
-            indicatorImage.widthAnchor.constraint(equalToConstant: 20)
-        ])
+        indicatorImage.image = isRising ?
+        UIImage(systemName: "chevron.compact.up", withConfiguration: UIImage.SymbolConfiguration(pointSize: 20, weight: .black)) :
+        UIImage(systemName: "chevron.compact.down", withConfiguration: UIImage.SymbolConfiguration(pointSize: 20, weight: .black))
+        
+        if isRising {
+            NSLayoutConstraint.activate([
+                indicatorImage.centerXAnchor.constraint(equalTo: self.centerXAnchor),
+                indicatorImage.topAnchor.constraint(equalTo: self.topAnchor,constant: 4),
+                indicatorImage.heightAnchor.constraint(equalToConstant: 20),
+                indicatorImage.widthAnchor.constraint(equalToConstant: 20)
+            ])
+        } else {
+            NSLayoutConstraint.activate([
+                indicatorImage.centerXAnchor.constraint(equalTo: self.centerXAnchor),
+                indicatorImage.bottomAnchor.constraint(equalTo: self.bottomAnchor,constant: -4),
+                indicatorImage.heightAnchor.constraint(equalToConstant: 20),
+                indicatorImage.widthAnchor.constraint(equalToConstant: 20)
+            ])
+        }
     }
-    
 }
